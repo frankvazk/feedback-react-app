1fa2a5757c80c63c78a37dc79c01811663af1571
Francisco Vazquez <francisco.v@brillio.com>
2022-11-15 15:27:01 -0600

    Rating Selector

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import RatingSelect from "./RatingSelect";
 import Button from "./shared/Button";
 import Card from "./shared/Card";
 
@@ -6,6 +7,7 @@ const FeedbackForm = () => {
   const [text, setText] = useState("");
   const [isDisabled, setIsDisabled] = useState(true);
   const [message, setMessage] = useState("");
+  const [rating, setRating] = useState(10);
 
   const handleTextChange = (e) => {
     let data = e.target.value;
@@ -27,6 +29,7 @@ const FeedbackForm = () => {
       <form>
         <h2>How you rate your service with us?</h2>
         {/** @todo - rating select component  */}
+        <RatingSelect rating={rating} setRating={setRating} />
         <div className="input-group">
           <input
             type="text"
